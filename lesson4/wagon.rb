b0ff5010eb1b11ade4b68e242fa633a309fc2982
medeiva91 <medeiva91@yandex.ru>
2019-05-06 20:59:18 +0200

    changed wagon.rb

--- lesson4/wagon.rb
+++ lesson4/wagon.rb
@@ -1,6 +1,21 @@
 class Wagon
-  attr_reader :type
+  include Info
+  attr_reader :type, :total_volume, :taken_volume
 
-  def initialize
+  def initialize(total_volume)
+    @total_volume = total_volume
+    @taken_volume = 0
+  end
+
+  def set_to_zero
+    @taken_volume = 0
+  end
+
+  def add_taken_place(value)
+    @taken_volume += value
+  end
+
+  def free_volume
+    total_volume - taken_volume
   end
 end
